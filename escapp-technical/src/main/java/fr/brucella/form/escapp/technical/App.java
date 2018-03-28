17b9ec170ca0087c822d91ee5fd3cef824e16d18
BRUCELLA2 <fouretfrederic@gmail.com>
2018-03-28 12:15:12 +0200

    Add technical module forgotten in last commit

---
+++ escapp-technical/src/main/java/fr/brucella/form/escapp/technical/App.java
@@ -0,0 +1,13 @@
+package fr.brucella.form.escapp.technical;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
