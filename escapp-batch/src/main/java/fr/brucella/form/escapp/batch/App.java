fb38a372e3727839dfd139e4ff476e80bc8c98ec
BRUCELLA2 <fouretfrederic@gmail.com>
2018-03-28 10:00:24 +0200

    Maven project 'escapp' creation with multi-modules architecture

---
+++ escapp-batch/src/main/java/fr/brucella/form/escapp/batch/App.java
@@ -0,0 +1,13 @@
+package fr.brucella.form.escapp.batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
